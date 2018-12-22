b932119d0f1763a3fb439b8a31b777994f01023b
TumeniNodes <tumeninodes@gmail.com>
2018-12-22 02:01:31 -0500

    clean up license infos

---
+++ models/nbe lua files/rgspro.lua
@@ -0,0 +1,25 @@
+-- GENERATED CODE
+-- Node Box Editor, version 0.9.0
+-- Namespace: test
+
+minetest.register_node("test:node_1", {
+	tiles = {
+		"default_wood.png",
+		"default_wood.png",
+		"default_wood.png",
+		"default_wood.png",
+		"default_wood.png",
+		"default_wood.png"
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, 0.375, 0.5, 0.5, 0.5},
+			{-0.5, -0.3125, 0.25, 0.5, 0.5, 0.5},
+			{-0.5, -0.125, 0.125, 0.5, 0.5, 0.5},
+		}
+	}
+})
+
