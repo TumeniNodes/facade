e8aec4d849c1846eb73aa5c3248a626cae0602e4
flux <25628292+fluxionary@users.noreply.github.com>
2019-08-28 22:21:01 +0000

    clean up some stuff; remove broken items

--- shaper.lua
+++ shaper.lua
@@ -6,231 +6,231 @@
 -- Balancing output per 1 input block with respect to apparent volume of output shape.
 -- All current shapes are added, but shapes not present in this table will still be produced
 -- one at a time — if that is the desired quantity, adding them is not required.
-local output_ratios       = {
-    bannerstone           = 1,
-    bannerstone_corner    = 1,
-    centerstone           = 1,
-    column                = 1,
-    column_corner         = 1,
-    corbel                = 1,
-    corbel_corner         = 1,
-    corbel_corner_inner   = 1,
-    carved_stone_a        = 1,
-    carved_stone_a_corner = 1,
-    rgspro                = 2,
-    rgspro_inner_corner   = 1,
-    rgspro_outer_corner   = 1,
-    corner_bricks         = 2,
+local output_ratios	   = {
+	bannerstone		   = 1,
+	bannerstone_corner	= 1,
+	centerstone		   = 1,
+	column				= 1,
+	column_corner		 = 1,
+	corbel				= 1,
+	corbel_corner		 = 1,
+	corbel_corner_inner   = 1,
+	carved_stone_a		= 1,
+	carved_stone_a_corner = 1,
+	rgspro				= 2,
+	rgspro_inner_corner   = 1,
+	rgspro_outer_corner   = 1,
+	corner_bricks		 = 2,
 }
 
 -- The material to be used for buttons when no material is actually loaded.
 -- It should be a generic material for which all the facade shapes are defined.
-local demo_material       = "default:stone"
+local demo_material	   = "default:stone"
 
 -- Whether the facade should obey area protection for the inventories (as machines in technic mod)
 -- or allow anybody to use them, but disallow the removal of machine itself (like circular saw in moreblocks)
 local protect_inventories = (minetest.settings:get("facade.protect_inventories") or "false"):lower() == "false"
 
 local function prepare_formspec(material_name)
-    local output          = string.gsub(material_name, "^.*:", "facade:")
-    local shaper_formspec = "size[8,11;]" ..
-            "label[0,0;" .. "Choose shape to produce:" .. "]" ..
+	local output		  = string.gsub(material_name, "^.*:", "facade:")
+	local shaper_formspec = "size[8,11;]" ..
+			"label[0,0;" .. "Choose shape to produce:" .. "]" ..
 
-            -- row 1, blocky shapes
-            "item_image_button[0,0.5;1,1;" .. output .. "_bannerstone" .. ";bannerstone; ]" ..
-            "item_image_button[1,0.5;1,1;" .. output .. "_bannerstone_corner" .. ";bannerstone_corner; ]" ..
-            "item_image_button[2,0.5;1,1;" .. output .. "_centerstone" .. ";centerstone; ]" ..
-            "item_image_button[3,0.5;1,1;" .. output .. "_carved_stone_a" .. ";carved_stone_a; ]" ..
-            "item_image_button[4,0.5;1,1;" .. output .. "_carved_stone_a_corner" .. ";carved_stone_a_corner; ]" ..
-            "item_image_button[5,0.5;1,1;" .. output .. "_column" .. ";column; ]" ..
-            "item_image_button[6,0.5;1,1;" .. output .. "_column_corner" .. ";column_corner; ]" ..
+			-- row 1, blocky shapes
+			"item_image_button[0,0.5;1,1;" .. output .. "_bannerstone" .. ";bannerstone; ]" ..
+			"item_image_button[1,0.5;1,1;" .. output .. "_bannerstone_corner" .. ";bannerstone_corner; ]" ..
+			"item_image_button[2,0.5;1,1;" .. output .. "_centerstone" .. ";centerstone; ]" ..
+			"item_image_button[3,0.5;1,1;" .. output .. "_carved_stone_a" .. ";carved_stone_a; ]" ..
+			"item_image_button[4,0.5;1,1;" .. output .. "_carved_stone_a_corner" .. ";carved_stone_a_corner; ]" ..
+			"item_image_button[5,0.5;1,1;" .. output .. "_column" .. ";column; ]" ..
+			"item_image_button[6,0.5;1,1;" .. output .. "_column_corner" .. ";column_corner; ]" ..
 
-            -- row 2, corbel
-            "item_image_button[0,1.5;1,1;" .. output .. "_corbel" .. ";corbel; ]" ..
-            "item_image_button[1,1.5;1,1;" .. output .. "_corbel_corner_inner" .. ";corbel_corner_inner; ]" ..
-            "item_image_button[2,1.5;1,1;" .. output .. "_corbel_corner" .. ";corbel_corner; ]" ..
+			-- row 2, corbel
+			"item_image_button[0,1.5;1,1;" .. output .. "_corbel" .. ";corbel; ]" ..
+			"item_image_button[1,1.5;1,1;" .. output .. "_corbel_corner_inner" .. ";corbel_corner_inner; ]" ..
+			"item_image_button[2,1.5;1,1;" .. output .. "_corbel_corner" .. ";corbel_corner; ]" ..
 
-            -- row 3, cornice
-            "item_image_button[0,2.5;1,1;" .. output .. "_rgspro" .. ";rgspro; ]" ..
-            "item_image_button[1,2.5;1,1;" .. output .. "_rgspro_inner_corner" .. ";rgspro_inner_corner; ]" ..
-            "item_image_button[2,2.5;1,1;" .. output .. "_rgspro_outer_corner" .. ";rgspro_outer_corner; ]"
+			-- row 3, cornice
+			"item_image_button[0,2.5;1,1;" .. output .. "_rgspro" .. ";rgspro; ]" ..
+			"item_image_button[1,2.5;1,1;" .. output .. "_rgspro_inner_corner" .. ";rgspro_inner_corner; ]" ..
+			"item_image_button[2,2.5;1,1;" .. output .. "_rgspro_outer_corner" .. ";rgspro_outer_corner; ]"
 
-    -- row for shapes which are not available for all materials
-    -- only one such shape exists so far, but more should be easy to add here
-    if minetest.registered_nodes[output .. "_corner_bricks"] then
-        shaper_formspec = shaper_formspec ..
-                "item_image_button[0,3.5;1,1;" .. output .. "_corner_bricks" .. ";corner_bricks; ]"
-    end
+	-- row for shapes which are not available for all materials
+	-- only one such shape exists so far, but more should be easy to add here
+	if minetest.registered_nodes[output .. "_corner_bricks"] then
+		shaper_formspec = shaper_formspec ..
+				"item_image_button[0,3.5;1,1;" .. output .. "_corner_bricks" .. ";corner_bricks; ]"
+	end
 
-    -- inventory part
-    shaper_formspec = shaper_formspec ..
-            "label[0, 5.5;" .. "In:" .. "]" ..
-            "list[current_name;src;1,5.5;1,1;]" ..
-            "label[3, 5.5;" .. "Out:" .. "]" ..
-            "list[current_name;dst;4,5.5;4,1;]" ..
+	-- inventory part
+	shaper_formspec = shaper_formspec ..
+			"label[0, 5.5;" .. "In:" .. "]" ..
+			"list[current_name;src;1,5.5;1,1;]" ..
+			"label[3, 5.5;" .. "Out:" .. "]" ..
+			"list[current_name;dst;4,5.5;4,1;]" ..
 
-            "list[current_player;main;0,7;8,4;]" ..
-            "listring[current_name;dst]" ..
-            "listring[current_player;main]" ..
-            "listring[current_name;src]" ..
-            "listring[current_player;main]"
+			"list[current_player;main;0,7;8,4;]" ..
+			"listring[current_name;dst]" ..
+			"listring[current_player;main]" ..
+			"listring[current_name;src]" ..
+			"listring[current_player;main]"
 
-    return shaper_formspec
+	return shaper_formspec
 end
 
 -- a simple check for compatibile materials
 local function check_material_applicability(material)
-    -- using centerstone node here, since it appears to be both one of the oldest
-    -- and defined for all materials as well, making it suitable for a quick check
+	-- using centerstone node here, since it appears to be both one of the oldest
+	-- and defined for all materials as well, making it suitable for a quick check
 	return minetest.registered_nodes[string.gsub(material, "^.*:", "facade:") .. "_centerstone"]
 end
 
 -- update the buttons to show shapes made from the actual material
 local function update_formspec_put(pos, listname, index, stack, player)
-    if protect_inventories and minetest.is_protected(pos, player:get_player_name()) then
-        return
-    end
+	if protect_inventories and minetest.is_protected(pos, player:get_player_name()) then
+		return
+	end
 
-    if listname ~= "src" then
-        return
-    end
+	if listname ~= "src" then
+		return
+	end
 
-    local meta          = minetest.get_meta(pos)
-    local material_name = stack:get_name()
+	local meta		  = minetest.get_meta(pos)
+	local material_name = stack:get_name()
 
-    if check_material_applicability(material_name) then
-        meta:set_string("formspec", prepare_formspec(material_name))
-    end
+	if check_material_applicability(material_name) then
+		meta:set_string("formspec", prepare_formspec(material_name))
+	end
 end
 
 -- update the buttons to show shapes made from demo material if all material is removed
 local function update_formspec_take(pos, listname, index, stack, player)
-    if protect_inventories and minetest.is_protected(pos, player:get_player_name()) then
-        return
-    end
+	if protect_inventories and minetest.is_protected(pos, player:get_player_name()) then
+		return
+	end
 
-    if listname ~= "src" then
-        return
-    end
+	if listname ~= "src" then
+		return
+	end
 
-    local meta = minetest.get_meta(pos)
-    local inv  = meta:get_inventory()
+	local meta = minetest.get_meta(pos)
+	local inv  = meta:get_inventory()
 
-    if inv:is_empty("src") then
-        meta:set_string("formspec", prepare_formspec(demo_material))
-    end
+	if inv:is_empty("src") then
+		meta:set_string("formspec", prepare_formspec(demo_material))
+	end
 end
 
 -- disallow putting in materials which are not supported
 local function check_inventory_put(pos, listname, index, stack, player)
-    if protect_inventories and minetest.is_protected(pos, player:get_player_name()) then
-        return 0
-    end
+	if protect_inventories and minetest.is_protected(pos, player:get_player_name()) then
+		return 0
+	end
 
-    if listname ~= "src" then
-        return 0
-    end
+	if listname ~= "src" then
+		return 0
+	end
 
-    local material_name = stack:get_name()
+	local material_name = stack:get_name()
 
-    if check_material_applicability(material_name) then
-        return stack:get_count()
-    else
-        return 0
-    end
+	if check_material_applicability(material_name) then
+		return stack:get_count()
+	else
+		return 0
+	end
 end
 
 local function check_inventory_take(pos, listname, index, stack, player)
-    if protect_inventories and minetest.is_protected(pos, player:get_player_name()) then
-        return 0
-    end
+	if protect_inventories and minetest.is_protected(pos, player:get_player_name()) then
+		return 0
+	end
 
-    if listname ~= "src" and listname ~= "dst" then
-        return 0
-    end
+	if listname ~= "src" and listname ~= "dst" then
+		return 0
+	end
 
-    return stack:get_count()
+	return stack:get_count()
 end
 
 local function check_inventory_move(pos, from_list, from_index, to_list, to_index, count, player)
-    if protect_inventories and minetest.is_protected(pos, player:get_player_name()) then
-        return 0
-    end
+	if protect_inventories and minetest.is_protected(pos, player:get_player_name()) then
+		return 0
+	end
 
-    if from_list ~= "dst" or to_list ~= "dst" then
-        return 0
-    end
+	if from_list ~= "dst" or to_list ~= "dst" then
+		return 0
+	end
 
-    return count
+	return count
 end
 
 -- process the form fields and convert source material to desired shapes
 local function form_handler(pos, formname, fields, sender)
-    if protect_inventories and minetest.is_protected(pos, sender:get_player_name()) then
-        return
-    end
+	if protect_inventories and minetest.is_protected(pos, sender:get_player_name()) then
+		return
+	end
 
-    if fields.quit then
-        return
-    end
+	if fields.quit then
+		return
+	end
 
-    local meta = minetest.get_meta(pos)
-    local inv  = meta:get_inventory()
+	local meta = minetest.get_meta(pos)
+	local inv  = meta:get_inventory()
 
-    if inv:is_empty("src") then
-        return
-    end
+	if inv:is_empty("src") then
+		return
+	end
 
-    local inputstack = inv:get_stack("src", 1)
-    local inputname  = inputstack:get_name()
+	local inputstack = inv:get_stack("src", 1)
+	local inputname  = inputstack:get_name()
 
-    for shape, _ in pairs(fields) do
-        local result = string.gsub(inputname, "^.*:", "facade:") .. "_" .. shape
+	for shape, _ in pairs(fields) do
+		local result = string.gsub(inputname, "^.*:", "facade:") .. "_" .. shape
 
-        -- one can never be overly paranoid, unlike the quick check before, this one is precise
-        if not minetest.registered_nodes[result] then
-            return
-        end
+		-- one can never be overly paranoid, unlike the quick check before, this one is precise
+		if not minetest.registered_nodes[result] then
+			return
+		end
 
-        -- output quantities are adjusted to preserve roughly same mass of resulting products
-        if output_ratios[shape] then
-            result = result .. " " .. output_ratios[shape]
-        end
+		-- output quantities are adjusted to preserve roughly same mass of resulting products
+		if output_ratios[shape] then
+			result = result .. " " .. output_ratios[shape]
+		end
 
-        if not inv:room_for_item("dst", result) then
-            return
-        end
+		if not inv:room_for_item("dst", result) then
+			return
+		end
 
-        inputstack:take_item(1)
-        inv:set_stack("src", 1, inputstack)
-        inv:add_item("dst", result)
-    end
+		inputstack:take_item(1)
+		inv:set_stack("src", 1, inputstack)
+		inv:add_item("dst", result)
+	end
 end
 
 local function check_removability(pos, player)
-    local meta  = minetest.get_meta(pos)
-    local owner = meta:set_string("owner")
-    local pname = player:get_player_name()
-    local inv   = meta:get_inventory()
+	local meta  = minetest.get_meta(pos)
+	local owner = meta:set_string("owner")
+	local pname = player:get_player_name()
+	local inv   = meta:get_inventory()
 
-    -- owner may always remove the device
-    if owner and owner ~= "" and pname and pname ~= "" and owner == pname then
-        if inv:is_empty("src") and inv:is_empty("dst") then
-            return true
-        else
-            return false
-        end
-    end
+	-- owner may always remove the device
+	if owner and owner ~= "" and pname and pname ~= "" and owner == pname then
+		if inv:is_empty("src") and inv:is_empty("dst") then
+			return true
+		else
+			return false
+		end
+	end
 
-    if minetest.is_protected(pos, player:get_player_name()) then
-        return false
-    end
+	if minetest.is_protected(pos, player:get_player_name()) then
+		return false
+	end
 
-    if inv:is_empty("src") and inv:is_empty("dst") then
-        return true
-    end
+	if inv:is_empty("src") and inv:is_empty("dst") then
+		return true
+	end
 
-    return false
+	return false
 end
 
 local function on_construct(pos)
@@ -253,61 +253,61 @@ local function after_place_node(pos, placer)
 end
 
 minetest.register_node("facade:shaper", {
-    description                   = "Shaper Machine",
-    drawtype                      = "nodebox",
-    node_box                      = {
-        type  = "fixed",
-        fixed = {
-            -- base
-            { -1 / 2, -1 / 2, -1 / 2, 1 / 2, -14 / 32, 1 / 2 },
-            -- back
-            { -8 / 32, -1 / 2, 12 / 32, 8 / 32, 1 / 2, 16 / 32 },
-            -- table
-            { -8 / 32, -4 / 32, -16 / 32, 8 / 32, 4 / 32, 16 / 32 },
-            -- rear table sliding support
-            { -16 / 32, -4 / 32, 12 / 32, 16 / 32, 4 / 32, 16 / 32 },
-            -- front table sliding support
-            { -8 / 32, -14 / 32, -12 / 32, 8 / 32, -4 / 32, -16 / 32 },
-            -- top tool beam
-            { -4 / 32, 16 / 32, -8 / 32, 4 / 32, 12 / 32, 12 / 32 },
-            -- cutter holder
-            { -2 / 32, 7 / 32, -2 / 32, 2 / 32, 14 / 32, 2 / 32 },
-            -- cutter
-            { -1 / 128, 6 / 32, -1 / 32, 1 / 128, 7 / 32, 1 / 32 },
-        },
-    },
-    selection_box                 = {
-        type  = "fixed",
-        fixed = {
-            { -1 / 2, -1 / 2, -1 / 2, 1 / 2, 1 / 2, 1 / 2 },
-        },
-    },
-    tiles                         = { "facade_shaper_top.png",
-                                      "facade_shaper_bottom.png",
-                                      "facade_shaper_right.png",
-                                      "facade_shaper_left.png",
-                                      "facade_shaper_back.png",
-                                      "facade_shaper_front.png" },
-    groups                        = { oddly_breakable_by_hand = 2, cracky = 3, dig_immediate = 1 },
-    paramtype                     = "light",
-    paramtype2                    = "facedir",
-    legacy_facedir_simple         = true,
-    on_construct                  = on_construct,
-    after_place_node              = after_place_node,
-    can_dig                       = check_removability,
-    allow_metadata_inventory_put  = check_inventory_put,
-    allow_metadata_inventory_take = check_inventory_take,
-    allow_metadata_inventory_move = check_inventory_move,
-    on_metadata_inventory_put     = update_formspec_put,
-    on_metadata_inventory_take    = update_formspec_take,
-    on_receive_fields             = form_handler,
+	description				   = "Shaper Machine",
+	drawtype					  = "nodebox",
+	node_box					  = {
+		type  = "fixed",
+		fixed = {
+			-- base
+			{ -1 / 2, -1 / 2, -1 / 2, 1 / 2, -14 / 32, 1 / 2 },
+			-- back
+			{ -8 / 32, -1 / 2, 12 / 32, 8 / 32, 1 / 2, 16 / 32 },
+			-- table
+			{ -8 / 32, -4 / 32, -16 / 32, 8 / 32, 4 / 32, 16 / 32 },
+			-- rear table sliding support
+			{ -16 / 32, -4 / 32, 12 / 32, 16 / 32, 4 / 32, 16 / 32 },
+			-- front table sliding support
+			{ -8 / 32, -14 / 32, -12 / 32, 8 / 32, -4 / 32, -16 / 32 },
+			-- top tool beam
+			{ -4 / 32, 16 / 32, -8 / 32, 4 / 32, 12 / 32, 12 / 32 },
+			-- cutter holder
+			{ -2 / 32, 7 / 32, -2 / 32, 2 / 32, 14 / 32, 2 / 32 },
+			-- cutter
+			{ -1 / 128, 6 / 32, -1 / 32, 1 / 128, 7 / 32, 1 / 32 },
+		},
+	},
+	selection_box				 = {
+		type  = "fixed",
+		fixed = {
+			{ -1 / 2, -1 / 2, -1 / 2, 1 / 2, 1 / 2, 1 / 2 },
+		},
+	},
+	tiles						 = { "facade_shaper_top.png",
+									  "facade_shaper_bottom.png",
+									  "facade_shaper_right.png",
+									  "facade_shaper_left.png",
+									  "facade_shaper_back.png",
+									  "facade_shaper_front.png" },
+	groups						= { oddly_breakable_by_hand = 2, cracky = 3, dig_immediate = 1 },
+	paramtype					 = "light",
+	paramtype2					= "facedir",
+	legacy_facedir_simple		 = true,
+	on_construct				  = on_construct,
+	after_place_node			  = after_place_node,
+	can_dig					   = check_removability,
+	allow_metadata_inventory_put  = check_inventory_put,
+	allow_metadata_inventory_take = check_inventory_take,
+	allow_metadata_inventory_move = check_inventory_move,
+	on_metadata_inventory_put	 = update_formspec_put,
+	on_metadata_inventory_take	= update_formspec_take,
+	on_receive_fields			 = form_handler,
 })
 
 minetest.register_craft({
 	output = 'facade:shaper',
 	recipe = {
-		{ '',                    'default:diamond',    '' },
+		{ '',					'default:diamond',	'' },
 		{ 'default:steel_ingot', 'default:steelblock', 'default:steel_ingot' },
-		{ '',                    'default:steelblock', '' },
+		{ '',					'default:steelblock', '' },
 	},
 })
